cca7b377d94059d584f16abef02b4f52e631eb20
Jerdan1980 <jeremytinana@gmail.com>
2018-04-05 14:33:34 -0400

    update files

--- driver.rb
+++ driver.rb
@@ -1,9 +1,14 @@
-require 'discordrb'
+require 'discordrb' #https://github.com/meew0/discordrb
+require 'rupy' #https://github.com/steeve/rupy
 
 bot = discordrb::Bot.new token: '<token here>'
 
 bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!
+  event.respond 'Pong!'
+end
+
+bot.message(with_text: '8ball') do |event|
+  event.respond 8ball.py
 end
 
 bot.run
