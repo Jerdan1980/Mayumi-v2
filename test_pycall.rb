b8458b3b35e98ea355b5f09c40aadd63561979f3
Jerdan1980 <jeremytinana@gmail.com>
2018-04-06 21:11:15 -0400

    clean junk

--- test_pycall.rb
+++
@@ -1,6 +0,0 @@
-require 'pycall/import'
-include PyCall::Import
-pyimport :math
-
-p "START"
-p math.sin(math.pi / 4)
--- test_rupy.rb
+++
@@ -1,6 +0,0 @@
-require "rupy"
-
-Rupy.start
-eight_ball = Rupy.import("test")
-p test.run().rubify
-Rupy.end
